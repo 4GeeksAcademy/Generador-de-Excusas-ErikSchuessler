e4ead6ad273bbd630100956ed4820ef727d1134c
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-10-23 03:37:56 +0000

    finished the project

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pajaro xd', 'Mi jefe'];
+
+  let action = ['se comió', 'orinó', 'chocó', 'rompió'];
+
+  let what = ['mi tarea', 'mi teléfono', 'el coche'];
+
+  let when = ['antes de clase', 'mientras dormía', 'mientras hacía ejercicio', 'durante el almuerzo', 'mientras rezaba'];
+  
+  //let whoAleatorio = seleccionarAleatorio(who);
+  //let actionAleatorio = seleccionarAleatorio(action);
+  //let whatAleatorio = seleccionarAleatorio(what);
+  //let whenAleatorio = seleccionarAleatorio(when);
+  let excuseElement = document.querySelector("#excuse");
+
+  excuseElement.innerHTML = `${seleccionarAleatorio(who)} ${seleccionarAleatorio(action)} ${seleccionarAleatorio(what)} ${seleccionarAleatorio(when)}`;
+};
+
+function seleccionarAleatorio(lista) {
+  let elementoAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[elementoAleatorio];
 };
